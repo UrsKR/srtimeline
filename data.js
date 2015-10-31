d9a0974bc859476cabda22001b8778c9adcb052c
Urs Reupke <ursreupke@gmail.com>
2015-10-31 21:30:34 +0100

    Formats events as JSON

--- data.js
+++ data.js
@@ -1,11 +1,30 @@
-var dataset = [
-	['2011-01-13', 'First UGE baby is born'],
-	['2011-12-24', 'The Awakening'],
-	['2021-04-30', 'Goblinization'],
-	['2029-02-08', 'The Crash of \'29'],
-	['2050-01-01', 'Present day of SR1'],
-	['2053-01-01', 'Present day of SR2'],
-	['2060-01-01', 'Present day of SR3'],
-	['2072-01-01', 'Present day of SR4'],
-	['2075-01-01', 'Present day of SR5']
+var events = [
+	{date:'2011-01-13',
+	 title:'First UGE baby is born'
+	},
+	{date:'2011-12-24',
+	 title:'The Awakening'
+	},
+	{date:'2021-04-30',
+	 title:'Goblinization'
+	},
+	{date:'2029-02-08',
+	 title:'The Crash of \'29'
+	},
+	{date:'2050-01-01', 
+	 title:'Present day of SR1'
+	},
+	{date:'2053-01-01', 
+	 title:'Present day of SR2'
+	},
+	{date:'2060-01-01', 
+	 title:'Present day of SR3'
+	},
+	{date:'2072-01-01', 
+	 title:'Present day of SR4'
+	},
+	{date:'2075-01-01', 
+	 title:'Present day of SR5'
+	},
 ];
+
