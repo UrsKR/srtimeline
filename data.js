61442e88e7786c879ab89db87abfe67bf55f98ba
Urs Reupke <ursreupke@gmail.com>
2015-10-31 18:19:58 +0100

    Separates Data and Document

---
+++ data.js
@@ -0,0 +1,11 @@
+var dataset = [
+							['2011-12-24', 'Awakening'],
+							['2012-05-23', 'UGE'],
+							['2020-03-32', 'Goblinization'],
+							['2029-04-01', 'First Crash'],
+							['2050-01-01', 'Present day of SR1'],
+							['2053-01-01', 'Present day of SR2'],
+							['2060-01-01', 'Present day of SR3'],
+							['2072-01-01', 'Present day of SR4'],
+							['2075-01-01', 'Present day of SR5']
+						  ];
