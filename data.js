dc3d28674688592e4b43a6f1e4a8ec81aaa0056d
Urs Reupke <ursreupke@gmail.com>
2015-10-31 22:19:47 +0100

    Introduces labels
    All entries with matching labels are marked when the cursor hovers on
    one of them.

--- data.js
+++ data.js
@@ -1,9 +1,21 @@
 var events = [
+	{date:'2006-11-17',
+	 title:'North Korea launches nukes at Japan',
+	 labels: ["war","asia"]
+	},
+	{date:'2008-11-04',
+	 title:'Jesse Garetty elected president of the U.S.',
+	 labels: ["politics","america"]
+	},
 	{date:'2011-01-13',
 	 title:'First UGE baby is born'
 	},
 	{date:'2011-12-24',
 	 title:'The Awakening'
+	},
+	{date:'2014-07-12',
+	 title:'Great Ghost Dance. Ghost Dance War begins.',
+	 labels: ["war","america"]
 	},
 	{date:'2021-04-30',
 	 title:'Goblinization'
@@ -12,19 +24,27 @@ var events = [
 	 title:'The Crash of \'29'
 	},
 	{date:'2050-01-01', 
-	 title:'Present day of SR1'
+	 title:'Present day of SR1',
+	 labels:['edition']
 	},
 	{date:'2053-01-01', 
-	 title:'Present day of SR2'
+	 title:'Present day of SR2',
+	 labels:['edition']
 	},
 	{date:'2060-01-01', 
-	 title:'Present day of SR3'
+	 title:'Present day of SR3',
+	 labels:['edition']
 	},
 	{date:'2072-01-01', 
-	 title:'Present day of SR4'
+	 title:'Present day of SR4',
+	 labels:['edition']
+	},
+	{date:'2072-03-15', 
+	 title:'Knight Errant takes over Seattle'
 	},
 	{date:'2075-01-01', 
-	 title:'Present day of SR5'
-	},
+	 title:'Present day of SR5',
+	 labels:['edition']
+	}
 ];
 
